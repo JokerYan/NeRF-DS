789bbd26cad9fbccf480dcdbd47dab9d9d69af6f
JokerYan <770792058@qq.com>
2022-07-27 15:47:04 +0800

    disable gpu visibility

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 export DATASET_PATH=/hdd/zhiwen/data/hypernerf/raw/americano/
 export EXPERIMENT_PATH=experiments/spec_exp01
-python train.py \
+CUDA_VISIBLE_DEVICES=-1 python train.py \
     --base_folder $EXPERIMENT_PATH \
     --gin_bindings="data_dir='$DATASET_PATH'" \
     --gin_configs configs/test_local.gin
