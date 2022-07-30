d0ad9211642a9987f005dd5a29d32f3a48741359
JokerYan <770792058@qq.com>
2022-07-30 09:59:49 +0800

    sort gt before render

--- utils/generate_gt_video.py
+++ utils/generate_gt_video.py
@@ -6,8 +6,13 @@ fps = 30
 data_dir = "/hdd/zhiwen/data/hypernerf/raw/americano"
 rgb_folder = os.path.join(data_dir, "rgb/1x")
 
-images = []
+image_paths = []
 for file_path in glob(rgb_folder + "/*"):
+  image_paths.append(file_path)
+image_paths = sorted(image_paths)
+
+images = []
+for file_path in image_paths:
   image = mediapy.read_image(file_path)
   images.append(image)
 
