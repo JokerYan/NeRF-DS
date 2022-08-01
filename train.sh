c82abff74e699d1b9d0dfcb64b093a3821eb4ee7
JokerYan <770792058@qq.com>
2022-08-01 16:19:36 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 export DATASET_PATH=/hdd/zhiwen/data/hypernerf/raw/americano/
-export EXPERIMENT_PATH=experiments/spec_exp01_base
-CUDA_VISIBLE_DEVICES=2 python train.py \
+export EXPERIMENT_PATH=experiments/spec_exp02_base
+CUDA_VISIBLE_DEVICES=2,3 python train.py \
     --base_folder $EXPERIMENT_PATH \
     --gin_bindings="data_dir='$DATASET_PATH'" \
     --gin_configs configs/test_local.gin
