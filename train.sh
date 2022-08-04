1ef938ff765633f37aefad0a9f8f3753f8378ef7
JokerYan <770792058@qq.com>
2022-08-04 16:57:56 +0800

    change data dir

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-#export DATASET_PATH=/hdd/zhiwen/data/hypernerf/raw/americano/
-export DATASET_PATH=/home/zwyan/3d_cv/data/hypernerf/raw/americano/
+export DATASET_PATH=/hdd/zhiwen/data/hypernerf/raw/americano/
+#export DATASET_PATH=/home/zwyan/3d_cv/data/hypernerf/raw/americano/
 export EXPERIMENT_PATH=experiments/spec_exp03_base
 CUDA_VISIBLE_DEVICES=0 python train.py \
     --base_folder $EXPERIMENT_PATH \
