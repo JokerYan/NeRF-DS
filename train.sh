0b03b63592aaba9bc4fa6bd7101d48dacc8fa396
JokerYan <770792058@qq.com>
2022-08-04 17:02:22 +0800

    reduce batch size

--- train.sh
+++ train.sh
@@ -4,5 +4,5 @@ export EXPERIMENT_PATH=experiments/spec_exp03_base
 CUDA_VISIBLE_DEVICES=1 python train.py \
     --base_folder $EXPERIMENT_PATH \
     --gin_bindings="data_dir='$DATASET_PATH'" \
-    --gin_configs configs/hypernerf_vrig_ap_2d.gin
+    --gin_configs configs/hypernerf_vrig_ap_2d_test.gin
 #    --gin_configs configs/test_local.gin
